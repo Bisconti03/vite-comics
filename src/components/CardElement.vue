ede727ad9b6f325625676a77db51174c1a38eb39
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-02-03 16:17:33 +0100

    implementato jumbotron e comics (versione base)

---
+++ src/components/CardElement.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        name: 'CardElement',
+        props : {
+            series : String,
+            img : String,
+        }
+    }
+
+</script>
+
+
+<template>
+
+    <div>
+
+        <div>
+            <img :src="img" alt="">
+        </div>
+
+        <h5>
+            {{series}}
+        </h5>
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+h5 {
+    
+    padding-top: 10px;
+}
+    
+</style>
